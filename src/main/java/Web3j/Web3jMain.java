5e33170b6ec46cd02ca21a8a06abdd46446af165
Marius <marius.weise@web.de>
2020-11-19 15:08:52 +0100

    Multiple changes and more readability

--- src/main/java/Web3j/Web3jMain.java
+++ src/main/java/Web3j/Web3jMain.java
@@ -12,33 +12,45 @@ import java.math.BigInteger;
 
 public class Web3jMain {
 
-        public Web3jMain(){
+    Web3j web3;
+    Credentials creds;
+    StaticGasProvider gasProvider;
+    NumberContract numberContract;
 
 
-            var web3 = Web3j.build(new HttpService("HTTP://127.0.0.1:7545"));
-            var creds = Credentials.create("44e746bdf08a465df59f94566643376d9f7a89128c6bac16562bb8da839b82a9");
+    public Web3jMain() throws Exception {
 
-            var gasProvider = new StaticGasProvider(BigInteger.valueOf(1000), BigInteger.valueOf(1000000));
+        //Provides a HttpService to local Ganache Blockchain and creates credentials from a private key from that blockchain
+        web3 = Web3j.build(new HttpService("HTTP://127.0.0.1:7545"));
+        creds = Credentials.create("44e746bdf08a465df59f94566643376d9f7a89128c6bac16562bb8da839b82a9");
+
+
+        //A Gasprovider for later use with ethereum network (local)
+        gasProvider = new StaticGasProvider(BigInteger.valueOf(1000), BigInteger.valueOf(1000000));
+
+
+        loadContract();
+        NumberContractTests tests = new NumberContractTests(numberContract);
+
+    }
+
+    //if the contract isn't deployed you need to deploy the contract (e.g. via bot command DeployContract) and specify the address here
+    // NumberContract.load( address...
+    private String loadContract() throws Exception {
+
+
+        if ((NumberContract.load("0xd0189c47d6fd02f8b6735a22d6f7f678bebdc029", web3, creds, gasProvider) != null)) {
+            System.out.println("hello");
+            numberContract = NumberContract.load("0xd0189c47d6fd02f8b6735a22d6f7f678bebdc029", web3, creds, gasProvider);
+            return "Contract loaded successfully";
+        } else {
+            System.out.println("Please deploy the contract first or check your given address");
+            return "Please deploy the contract first or check your given address";
         }
 
 
-
-
-
-
-        //NumberContract numberContract = NumberContract.deploy(web3, creds, gasProvider).send();
-        NumberContract numberContract = NumberContract.load("0xd0189c47d6fd02f8b6735a22d6f7f678bebdc029",web3, creds, gasProvider);
-
-        System.out.println(numberContract.getContractAddress());
-
-
-        numberContract.storeNumber(BigInteger.valueOf(15)).send();
-        System.out.println(numberContract.giveStoredNumber().send());
-
-        numberContract.storeNumber(BigInteger.valueOf(10)).send();
-        System.out.println(numberContract.giveStoredNumber().send());
-
-
-
     }
+
+
+}
 
