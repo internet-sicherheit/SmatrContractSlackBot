6d6d7595bc9848486a97649aa333d4d9f208cdd8
Marius <marius.weise@web.de>
2020-12-03 17:58:36 +0100

    Changed package directory

--- src/main/java/org/web3j/tx/response/PollingTransactionReceiptProcessor.java
+++ src/main/java/org/web3j/tx/response/PollingTransactionReceiptProcessor.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package Web3j;
+package org.web3j.tx.response;
 
 import java.io.IOException;
 import java.util.Optional;
