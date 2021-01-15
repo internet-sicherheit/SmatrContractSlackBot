17ec4793017ff5ad65c6afad03cb7da7707b25ce
Marius <marius.weise@web.de>
2021-01-15 02:51:24 +0100

    New Classes for Slack Bot Messaging

---
+++ src/main/java/Web3j/SlackMessage.java
@@ -0,0 +1,11 @@
+package Web3j;
+
+import java.io.Serializable;
+
+
+public class SlackMessage implements Serializable {
+
+    private String username;
+    private String text;
+    private String icon_emoji;
+}
---
+++ src/main/java/Web3j/SlackUtils.java
@@ -0,0 +1,34 @@
+package Web3j;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+
+public class SlackUtils {
+    private static String slackWebhookUrl = "https://hooks.slack.com/services/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";
+
+    public static void sendMessage(SlackMessage message) {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(slackWebhookUrl);
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(message);
+
+            StringEntity entity = new StringEntity(json);
+            httpPost.setEntity(entity);
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            client.execute(httpPost);
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
